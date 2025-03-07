b21345d397215099a1238cd450a10b4d4be63946
Chia-I Wu <olvaffe@gmail.com>
2025-03-07 14:01:10 -0800

    report cpu idle

--- src/prometheus.rs
+++ src/prometheus.rs
@@ -1,23 +1,29 @@
 // Copyright 2025 Google LLC
 // SPDX-License-Identifier: MIT
 
-use prometheus::{Encoder, IntCounter, Opts, TextEncoder, register_int_counter};
+use prometheus::{Encoder, IntGauge, Opts, TextEncoder, register_int_gauge};
+
+const NAMESPACE: &str = "home_router";
 
 pub struct Prom {
     encoder: TextEncoder,
-    counter: IntCounter,
+
+    cpu_idle_ms: IntGauge,
 }
 
 impl Prom {
     pub fn new() -> Self {
-        let test_opts = Opts::new("counter", "test counter")
-            .namespace("home_router")
-            .subsystem("test");
-        let test_counter = register_int_counter!(test_opts).unwrap();
+        let encoder = TextEncoder::new();
+        let cpu_idle_ms = register_int_gauge!(
+            Opts::new("idle_ms", "CPU idle time")
+                .namespace(NAMESPACE)
+                .subsystem("cpu")
+        )
+        .unwrap();
 
         Prom {
-            encoder: TextEncoder::new(),
-            counter: test_counter,
+            encoder,
+            cpu_idle_ms,
         }
     }
 
@@ -26,7 +32,12 @@ impl Prom {
     }
 
     pub fn update(&self) {
-        self.counter.inc();
+        self.update_cpu();
+    }
+
+    fn update_cpu(&self) {
+        let stat = crate::procfs::parse_stat().expect("failed to parse /proc/stat");
+        self.cpu_idle_ms.set(stat.idle_ms.try_into().unwrap());
     }
 
     pub fn gather(&self) -> Vec<u8> {
