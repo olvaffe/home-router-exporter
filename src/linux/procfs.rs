8e3c4ebe8ccc4f1a7bfd4814d1005b9806c39c85
Chia-I Wu <olvaffe@gmail.com>
2025-03-08 17:11:04 -0800

    clean up procfs

--- src/linux/procfs.rs
+++ src/linux/procfs.rs
@@ -2,14 +2,12 @@
 // SPDX-License-Identifier: MIT
 
 use anyhow::{Context, Result, anyhow};
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::path::Path;
+use std::io::BufRead;
 
-pub struct ProcStat {
-    pub user_ms: u64,
-    pub system_ms: u64,
-    pub idle_ms: u64,
+pub struct ProcDiskStat {
+    pub name: String,
+    pub read_bytes: u64,
+    pub write_bytes: u64,
 }
 
 pub struct ProcMemInfo {
@@ -19,10 +17,10 @@ pub struct ProcMemInfo {
     pub swap_free_kb: u64,
 }
 
-pub struct ProcDiskStat {
-    pub name: String,
-    pub read_bytes: u64,
-    pub write_bytes: u64,
+pub struct ProcStat {
+    pub user_ms: u64,
+    pub system_ms: u64,
+    pub idle_ms: u64,
 }
 
 pub struct ProcMountInfo {
@@ -33,7 +31,7 @@ pub struct ProcMountInfo {
     pub avail: u64,
 }
 
-pub fn parse_diskstats_line(line: &str) -> Result<ProcDiskStat> {
+fn parse_diskstats_line(line: &str) -> Result<ProcDiskStat> {
     // 0:major 1:minor 2:name
     // 3:r_completed 4:r_merged 5:r_sectors 6:r_time
     // 7:w_completed 8:w_merged 9:w_sectors 10:w_time
@@ -55,7 +53,7 @@ pub fn parse_diskstats_line(line: &str) -> Result<ProcDiskStat> {
     })
 }
 
-pub fn parse_mountinfo_line(line: &str) -> Result<(&str, &str)> {
+fn parse_pid_mountinfo_line(line: &str) -> Result<(&str, &str)> {
     // 0:id 1:parent_id 2:major:minor 3:root 4:mountpoint 5:options
     // optional fields... n:seperator
     // n+1:fs_type n+2:src n+3:super
@@ -76,27 +74,18 @@ pub fn parse_mountinfo_line(line: &str) -> Result<(&str, &str)> {
 }
 
 impl super::Linux {
-    pub fn parse_stat(&self) -> Result<ProcStat> {
-        let mut reader = self.procfs_open("stat")?;
+    pub fn parse_diskstats(&self) -> Result<Vec<ProcDiskStat>> {
+        let reader = self.procfs_open("diskstats")?;
 
-        let mut line = String::new();
-        reader.read_line(&mut line).context("failed to read stat")?;
-
-        // 0:type 1:user 2:nice 3:system 4:idle 5:iowait
-        let cols: Vec<&str> = line.split_ascii_whitespace().collect();
-        if cols.len() < 5 || cols[0] != "cpu" {
-            return Err(anyhow!("failed to parse stat"));
+        let mut stats = Vec::new();
+        for line in reader.lines() {
+            let stat = parse_diskstats_line(&line?)?;
+            if stat.read_bytes != 0 || stat.write_bytes != 0 {
+                stats.push(stat)
+            }
         }
-        let [user_ms, system_ms, idle_ms] = [cols[1], cols[3], cols[4]].map(|col| {
-            let ticks: u64 = col.parse().unwrap_or(0);
-            ticks * 1000 / self.sysconf_user_hz / self.sysconf_nproc
-        });
 
-        Ok(ProcStat {
-            user_ms,
-            system_ms,
-            idle_ms,
-        })
+        Ok(stats)
     }
 
     pub fn parse_meminfo(&self) -> Result<ProcMemInfo> {
@@ -138,18 +127,27 @@ impl super::Linux {
         })
     }
 
-    pub fn parse_diskstats(&self) -> Result<Vec<ProcDiskStat>> {
-        let reader = self.procfs_open("diskstats")?;
+    pub fn parse_stat(&self) -> Result<ProcStat> {
+        let mut reader = self.procfs_open("stat")?;
 
-        let mut stats = Vec::new();
-        for line in reader.lines() {
-            let stat = parse_diskstats_line(&line?)?;
-            if stat.read_bytes != 0 || stat.write_bytes != 0 {
-                stats.push(stat)
-            }
+        let mut line = String::new();
+        reader.read_line(&mut line).context("failed to read stat")?;
+
+        // 0:type 1:user 2:nice 3:system 4:idle 5:iowait
+        let cols: Vec<&str> = line.split_ascii_whitespace().collect();
+        if cols.len() < 5 || cols[0] != "cpu" {
+            return Err(anyhow!("failed to parse stat"));
         }
+        let [user_ms, system_ms, idle_ms] = [cols[1], cols[3], cols[4]].map(|col| {
+            let ticks: u64 = col.parse().unwrap_or(0);
+            ticks * 1000 / self.sysconf_user_hz / self.sysconf_nproc
+        });
 
-        Ok(stats)
+        Ok(ProcStat {
+            user_ms,
+            system_ms,
+            idle_ms,
+        })
     }
 
     pub fn parse_self_mountinfo(&self) -> Result<Vec<ProcMountInfo>> {
@@ -158,7 +156,7 @@ impl super::Linux {
         let mut infos: Vec<ProcMountInfo> = Vec::new();
         for line in reader.lines() {
             let line = line?;
-            let (src, dst) = parse_mountinfo_line(&line)?;
+            let (src, dst) = parse_pid_mountinfo_line(&line)?;
             if !src.starts_with("/") {
                 continue;
             }
