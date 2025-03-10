25750d650ed3e0fdda1e88c3b8fa9fb0d4dec81e
Chia-I Wu <olvaffe@gmail.com>
2025-03-09 20:21:16 -0700

    test ping 3

--- src/ping.rs
+++ src/ping.rs
@@ -3,45 +3,61 @@
 
 use crate::prometheus::Prom;
 use anyhow::Result;
-use std::{net, sync};
+use std::{net, sync, time};
 
 pub struct Ping {
     notify: sync::Arc<tokio::sync::Notify>,
+
+    hosts: sync::Arc<sync::Mutex<Vec<net::IpAddr>>>,
 }
 
-async fn test_ping(notify: sync::Arc<tokio::sync::Notify>) -> Result<()> {
-    let host_v4 = net::IpAddr::V4(net::Ipv4Addr::LOCALHOST);
+struct Roundtrip {
+    host: net::IpAddr,
+    duration: time::Duration,
+}
+
+async fn test_ping(
+    notify: sync::Arc<tokio::sync::Notify>,
+    hosts: sync::Arc<sync::Mutex<Vec<net::IpAddr>>>,
+) -> Result<()> {
     let config_v4 = surge_ping::Config::builder().build();
     let client_v4 = surge_ping::Client::new(&config_v4)?;
-    let mut pinger_v4 = client_v4
-        .pinger(host_v4, surge_ping::PingIdentifier(0))
-        .await;
 
-    let host_v6 = net::IpAddr::V6(net::Ipv6Addr::LOCALHOST);
     let config_v6 = surge_ping::Config::builder()
         .kind(surge_ping::ICMP::V6)
         .build();
     let client_v6 = surge_ping::Client::new(&config_v6)?;
-    let mut pinger_v6 = client_v6
-        .pinger(host_v6, surge_ping::PingIdentifier(0))
-        .await;
 
+    let ident = surge_ping::PingIdentifier(0);
     let payload = [0u8; 56];
     let mut seqno = surge_ping::PingSequence(0);
     loop {
         notify.notified().await;
 
-        let (reply_v4, reply_v6) = tokio::join!(
-            pinger_v4.ping(seqno, &payload),
-            pinger_v6.ping(seqno, &payload)
-        );
+        let hosts = hosts.lock().unwrap().clone();
+
+        let mut pingers = Vec::new();
+        for host in &hosts {
+            let pinger = match host {
+                net::IpAddr::V4(_) => client_v4.pinger(*host, ident),
+                net::IpAddr::V6(_) => client_v6.pinger(*host, ident),
+            }
+            .await;
+            pingers.push(pinger);
+        }
+
+        let mut futures = Vec::new();
+        for pinger in &mut pingers {
+            futures.push(pinger.ping(seqno, &payload));
+        }
+
         seqno.0 += 1;
 
-        if let Ok((_, dur)) = reply_v4 {
-            println!("{:?}: {:?}", pinger_v4.host, dur);
-        }
-        if let Ok((_, dur)) = reply_v6 {
-            println!("{:?}: {:?}", pinger_v6.host, dur);
+        let replies = futures::future::join_all(futures).await;
+        for (host, reply) in std::iter::zip(hosts, replies) {
+            if let Ok((_, dur)) = reply {
+                println!("{:?}: {:?}", host, dur);
+            }
         }
     }
 }
@@ -49,8 +65,12 @@ async fn test_ping(notify: sync::Arc<tokio::sync::Notify>) -> Result<()> {
 impl Ping {
     pub fn new() -> Self {
         let notify = sync::Arc::new(tokio::sync::Notify::new());
-        tokio::task::spawn(test_ping(notify.clone()));
-        Ping { notify }
+        let hosts = sync::Arc::new(sync::Mutex::new(vec![
+            net::IpAddr::V4(net::Ipv4Addr::LOCALHOST),
+            net::IpAddr::V6(net::Ipv6Addr::LOCALHOST),
+        ]));
+        tokio::task::spawn(test_ping(notify.clone(), hosts.clone()));
+        Ping { notify, hosts }
     }
 
     pub fn collect(&self, _prom: &Prom) {
